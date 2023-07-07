e84199908ff799f9e02294eb3ca4186485f1f814
vivsub761 <78286482+vivsub761@users.noreply.github.com>
2023-07-06 21:30:42 -0700

    Update server.py

--- server.py
+++ server.py
@@ -1,12 +1,10 @@
 from flask import Flask, request
 import json
 import base64
-from updateDocumentation import astUpdater
+from updateDocumentation import astUpdater, NestedFunctionFinder
+import ast
 from waitress import serve
 app = Flask(__name__)
-
-
-
 
 @app.route("/", methods = ["Post"])
 def getDocumentation():
@@ -14,10 +12,13 @@ def getDocumentation():
         data comes from front end in format
         {encoded_file_data: b64encodedString}
     '''
-    print("REQUEST RECEIVED")
     data = request.get_json()
     decoded_data = base64.b64decode(data["encoded_file_data"])
-    documentationFetcher = astUpdater(decoded_data)
+    tree = ast.parse(decoded_data)
+    findNested = NestedFunctionFinder(tree)
+    findNested.visit(findNested.tree)
+    print([node.name for node in findNested.nested])
+    documentationFetcher = astUpdater(tree, findNested.nested)
     return documentationFetcher.getDocumentation()
 
 
